f2ac159d22bb23175c067275ba4b9f6cab171e91
Yuetong Lu <yuetong@usc.edu>
2020-08-22 23:54:10 -0400

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream> 
+#include <array>
+using namespace std;
+int main()
+{
+    //printf("Hello World");
+    // int x=10;
+    // int y=x+5; 
+    
+    // cout<<"Hello\n";
+    //std::cout << "Hello World!"; 
+    //return 0;
+    // int number, number2;
+
+    // cout << "Enter an integer: ";
+    // cin >> number>>number2;
+
+    // cout << "You entered " << number;    
+   
+    
+    //variable = (condition)? true : false;
+    int TorF= (5>2)? 5:2;
+    cout<<TorF<<endl; 
+    
+    string s="1234";
+    cout<<s<<endl;
+    
+    
+    int numArray[]={1,1,1,1,1,1};
+    cout<<numArray.size()<<endl;
+    return 0;
+}
+
+
+
+
